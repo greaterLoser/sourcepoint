4b2ad50dce217d07f0b4704ed6644286feaa36ef
greaterLoser <greaterLoser@tutanota.com>
2020-01-31 16:11:03 -0700

    temp fix

---
+++ src/pages/xox2.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+const SecondPage = () => (
+  <Layout>
+	<div id="07b02207602203a02205806205104a05407305707203504705907402207d" 
+	style={{ 
+		height: "450px",
+		width: "720px" 
+	}}></div>
+  </Layout>
+)
+
+export default SecondPage
---
+++ src/pages/xox3.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+const SecondPage = () => (
+  <Layout>
+	<div id="07b02207602203a02203506105903907707003204f06307304f06802207d" 
+	style={{ 
+		height: "450px",
+		width: "720px" 
+	}}></div>
+  </Layout>
+)
+
+export default SecondPage
