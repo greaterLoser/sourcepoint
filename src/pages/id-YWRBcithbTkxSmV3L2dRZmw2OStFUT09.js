76c628c25fc9b99abf837fbf7b56eb6499e37580
greaterLoser <greaterLoser@tutanota.com>
2020-01-31 13:27:47 -0700

    pageUp

---
+++ src/pages/id-YWRBcithbTkxSmV3L2dRZmw2OStFUT09.js
@@ -0,0 +1,27 @@
+import React from "react"
+let contStyle =
+	{position: "relative",
+	overflow: "hidden",
+	paddingTop: "56.25%",
+	backgroung: "black"
+	};
+
+let iframeStyle =
+	{
+	position: "absolute",
+	top: "0",
+	left: "0",
+	width: "100%",
+	height: "75%",
+	border: "0",
+	backgroung: "black"
+	};
+
+
+const SecondPage = () => (
+    <div style={contStyle}>
+      <iframe title="Sugma" style={iframeStyle} src="https://hqq.tv/player/embed_player.php?vid=YWRBcithbTkxSmV3L2dRZmw2OStFUT09&autoplay=no" allowfullscreen>></iframe>
+    </div>
+)
+
+export default SecondPage
